30c71bea43250c0da391dd48e4a4cd54194f4613
arpit <arpit.jain@neosoftmail.com>
2026-03-29 18:09:46 +0530

    advanced routes

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const app = express();
 
+// app.use("/test", (req, res) => {
+//   res.send("hello2");
+// });
 
-app.use("/test", (req, res) => {
-  res.send("hello2");
+// app.get("/arpit", (req, res) => {
+//   res.send("hello3");
+// });
+
+// app.use("/", (req, res) => {
+//   res.send("hello1");
+// });
+
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params)
+  res.send({ name: "Arpit", lname: "jain" });
 });
 
-app.get("/arpit", (req, res) => {
-  res.send("hello3");
+app.get("/user", (req, res) => {
+    console.log(req.query)
+  res.send({ name: "Arpit", lname: "jain" });
 });
 
-app.use("/", (req, res) => {
-  res.send("hello1");
+app.post("/user", (req, res) => {
+  //we can do anything here like saving of data into database
+
+  res.send("data savexd successfully");
 });
 
 app.listen(3000, () => console.log("s/v started successfully"));
